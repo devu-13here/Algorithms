3363d39cf0a1d2abfabf95a6fe68bf7e8e6df2d8
Devanjali Patel <150452256+devu-13here@users.noreply.github.com>
2024-06-23 07:32:36 +0530

    FractionaL Knapsack

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 struct Job{
@@ -8,32 +9,40 @@ struct Job{
 	
 };
 
-bool comparison(Job a,Job b)
-{
-	return (a.profit> b.profit);
+bool comparison(Job a, Job b){
+	return (a.profit > b.profit);
 }
 
-void printJobScheduling(Job arr[],int n)
-{
-	sort(arr, arr+n ,comparison);
+void printJobScheduling(Job arr[], int n){
+	sort(arr, arr+n, comparison);
 	
 	int result[n];
 	bool slot[n];
 	
-	for(int i=0;i<n;i++)
-	 slot[i]=false;
+	for(int i=0; i<n; i++)
+		slot[i] = false;
 	 
-	for(int i=0 ;i<n;i++)
-	   
-	   for(int j=min(n,arr[i].dead)-1; j>=0 ;j--){
+	for(int i=0; i<n; i++){
+	   for(int j = min(n, arr[i].dead) - 1; j >= 0; j--){
 	   	
-	   	if(slot[j]== false)
-	   	{result[j]==i;
-	   	slot[j]==true;
-	   	break;
-		   }
-	   }
+	   	if(slot[j] == false)
+	   	{
+	   		result[j] = i;
+	   		slot[j] = true;
+	   		break;
+		}
+	}
 }
 
-for(int i=0 ; i<n; i++)
-   
+for(int i=0; i<n; i++)
+	cout << arr[result[i]].id << " ";
+}
+
+int main(){
+	Job arr[] = { {'a', 2, 100}, {'b', 1, 19}, {'c', 2, 27},
+                  {'d', 1, 25}, {'e', 3, 15} };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "The sequence of jobs with maximum profit: ";
+    printJobScheduling(arr, n);
+    return 0;
+}
